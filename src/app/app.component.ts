0daf580d875fa0d5a9767d96a861658305e54064
GAVILALBA <GAVILALBA@TCEMT014952>
2025-07-22 09:30:34 -0400

    mudanças header, footer e botao topo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+// src/app/app.component.ts
+
+import { Component, HostListener, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'truck-empreendimentos';
+
+  // Adicione a referência ao botão
+  @ViewChild('scrollToTopBtn') scrollToTopBtn!: ElementRef;
+
+  // Adicione o ouvinte de rolagem
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (window.scrollY > 300) { // Mostra o botão após rolar 300px
+      this.scrollToTopBtn.nativeElement.classList.add('show');
+    } else {
+      this.scrollToTopBtn.nativeElement.classList.remove('show');
+    }
+  }
+
+  // Adicione o método para rolar ao topo
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
