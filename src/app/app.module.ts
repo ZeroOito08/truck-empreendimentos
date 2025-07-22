af8754ffcbb1e4658a0ca77b400d7233327d0d16
GAVILALBA <GAVILALBA@TCEMT014952>
2025-07-22 10:49:01 -0400

    ajuste menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { NavComponent } from './shared/nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { ContatoComponent } from './contato/contato.component';
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
@@ -14,7 +13,6 @@ import { FooterComponent } from './shared/footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent,
     HomeComponent,
     ContatoComponent,
     SobreNosComponent,
